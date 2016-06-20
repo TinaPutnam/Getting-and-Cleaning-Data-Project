26e839f24062f836f484ee6233509d9aa7195377
TinaPutnam <tina.putnam@smiths-medical.com>
2016-06-20 11:47:21 -0400

    Add files via upload

--- run_anaylsis.R
+++ run_anaylsis.R
@@ -15,6 +15,9 @@
  # 4. Appropriately label the data set with descriptive activity names. 
 
 # 5. Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+# Set Working Directory 
+# Load  Data to working Directory
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
   
  # Read in the data from files 
 
